1e1314e1e6e12e6238b18a8d243741f74cf028ec
niangkouss <takeevernote@163.com>
2018-02-08 16:59:32 +0800

    添加新闻换成百度的富文本编辑器EUDITOR

--- static/utf8-php/php/controller.php
+++ static/utf8-php/php/controller.php
@@ -1,6 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin: *'); //设置http://www.baidu.com允许跨域访问
-header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With'); //设置允许的跨域header
+//header('Access-Control-Allow-Origin: *'); //设置http://www.baidu.com允许跨域访问
+//header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With'); //设置允许的跨域header
 date_default_timezone_set("Asia/chongqing");
 error_reporting(E_ERROR);
 header("Content-Type: text/html; charset=utf-8");
